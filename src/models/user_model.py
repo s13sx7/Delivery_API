e4ebdfcb01bf84ca4c8f812d4ccc01c12169d9ef
Andrey <s13sx7@gmail.com>
2025-08-15 21:18:05 +0700

    add unique in name and email column

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 from pydantic import EmailStr
 class User(Base):
-    name: Mapped[str] = mapped_column(nullable=False)
-    email: Mapped[EmailStr] = mapped_column(String(255), nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[EmailStr] = mapped_column(String(255), nullable=False, unique=True)
 
     hashed_password: Mapped[str] = mapped_column(nullable=False) 
